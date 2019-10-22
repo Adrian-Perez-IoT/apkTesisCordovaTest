206bb7dfffa5e58f73566d072bef7cbef62c56cb
bandydos <perezjadrian@gmail.com>
2019-10-22 18:13:54 -0300

    se agrego la vista de tabla para el monitoreo en tiempo real

---
+++ src/components/Monitoreo.vue
@@ -0,0 +1,147 @@
+<template>
+  <v-data-table
+    :headers="headers"
+    :items="desserts"
+    class="elevation-1"
+  >
+    <template v-slot:item.mq2="{ item }">
+      <v-chip :color="getColorMq2(item.mq2)" dark outlined>{{ item.mq2 }} </v-chip>
+    </template>
+    <template v-slot:item.mag="{ item }">
+      <v-chip :color="getColorMag(item.mag) " dark outlined>{{ item.mag }} </v-chip>
+    </template>    
+    <template v-slot:item.pir1="{ item }">
+      <v-chip :color="getColorPir1(item.pir1) " dark outlined>{{ item.pir1 }} </v-chip>
+    </template>    
+    <template v-slot:item.pir2="{ item }">
+      <v-chip :color="getColorPir2(item.pir2) " dark outlined>{{ item.pir2 }} </v-chip>
+    </template>    
+  </v-data-table>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        headers: [
+          {
+            text: 'hour:minute:second',
+            align: 'left',
+            sortable: false,
+            value: 'hhmmss',
+          },
+          { text: 'Toxic gas', value: 'mq2' },
+          { text: 'Door status', value: 'mag' },
+          { text: 'Movement (home backyard)', value: 'pir1'},
+          { text: 'Movement (home living room)', value: 'pir2' }          
+        ],
+        desserts: [
+          {
+            hhmmss: '23:15:00',
+            mq2: 'normal',
+            mag: 'closed',
+            pir1: 'no',
+            pir2: 'no',
+          },
+          {
+            hhmmss: '23:15:01',
+            mq2: 'warning',
+            mag: 'closed',
+            pir1: 'no',
+            pir2: 'no',
+          },
+          {
+            hhmmss: '23:15:02',
+            mq2: 'warning',
+            mag: 'closed',
+            pir1: 'no',
+            pir2: 'no',
+          },
+          {
+            hhmmss: '23:15:03',
+            mq2: 'warning',
+            mag: 'open',
+            pir1: 'no',
+            pir2: 'yes',
+          },
+          {
+            hhmmss: '23:15:04',
+            mq2: 'warning',
+            mag: 'open',
+            pir1: 'yes',
+            pir2: 'no',
+          },
+          {
+            hhmmss: '23:15:05',
+            mq2: 'warning',
+            mag: 'open',
+            pir1: 'yes',
+            pir2: 'yes',
+          },
+          {
+            hhmmss: '23:15:06',
+            mq2: 'warning',
+            mag: 'open',
+            pir1: 'no',
+            pir2: 'no',
+          },
+          {
+            hhmmss: '23:15:07',
+            mq2: 'danger',
+            mag: 'closed',
+            pir1: 'no',
+            pir2: 'no',
+          },
+          {
+            hhmmss: '23:15:08',
+            mq2: 'danger',
+            mag: 'closed',
+            pir1: 'no',
+            pir2: 'yes',
+          },
+          {
+            hhmmss: '23:15:09',
+            mq2: 'danger',
+            mag: 'closed',
+            pir1: 'yes',
+            pir2: 'no',
+          },
+        ],
+      }
+    },
+    methods: {
+      getColorMq2 (mq2) {
+        if (mq2 === 'danger') {
+          return 'red'
+        }
+        else {
+          if (mq2 === 'warning') {
+            return 'orange'
+          }          
+          else {
+            return 'green'
+          }
+        }        
+      },
+      getColorMag(mag) {
+        if (mag === 'open')
+          return 'orange'        
+        else 
+            return 'green'            
+      },
+      getColorPir1(pir1) {
+        if (pir1 === 'yes')
+          return 'orange'        
+        else 
+            return 'green'            
+      },
+      getColorPir2(pir2) {
+        if (pir2 === 'yes')
+          return 'orange'        
+        else 
+            return 'green'            
+      },
+      
+    },
+  }
+</script>
---
+++ src/components/ToolBar.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <v-app-bar app class="red darken-1">
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-spacer></v-spacer>
+      <v-btn class="success" small>Ingreso</v-btn>
+      <v-btn class="danger" small>Salir</v-btn>
+    </v-app-bar>
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-list-item>
+        <v-list-item-avatar>
+          <v-img src="https://randomuser.me/api/portraits/men/78.jpg"></v-img>
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title>John Leider</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense>
+
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon> {{item.icon}} </v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title> {{item.title}}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <!-- <v-navigation-drawer app v-model="drawer" temporary dark >
+        <v-layout mt-4 column align-center>
+            <v-flex>
+                <v-avatar>
+                    <img src="https://randomuser.me/api/portraits/men/85.jpg" alt="alt">
+                </v-avatar>
+            </v-flex>
+            <v-flex xs1>
+                <p class="white--text mt-3 headline ">Juan Pérez</p>
+                
+            </v-flex>
+        </v-layout>
+    </v-navigation-drawer> -->
+  </div>
+</template>
+
+<script>
+export default {
+    name: "ToolBar",
+    data(){
+        return {
+            drawer:null,
+            items: [
+                { title: 'Generar informes', icon: 'mdi-xbox-controller' },                
+                { title: 'Programar funcionamiento', icon: 'mdi-xbox-controller' },
+                { title: 'Administrar usuarios', icon: 'mdi-watch' },
+            ]
+        }
+    }
+}
+</script>
