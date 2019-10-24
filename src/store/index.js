c227d881bc83cfdade4f30e5a3d2b5adb1d8c5a2
bandydos <perezjadrian@gmail.com>
2019-10-24 18:24:13 -0300

    se adapto la vista de tabla para que pueda leer los datos directamente desde un array

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,24 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    sensorsReadings:[
+      {hhmmss:'00:46:00', mag:'closed', mq2:'normal', pir1:'no', pir2:'no'},
+      {hhmmss:'00:46:01', mag:'open', mq2:'warning', pir1:'no', pir2:'yes'},
+      {hhmmss:'00:46:02', mag:'closed', mq2:'danger', pir1:'yes', pir2:'no'},
+      {hhmmss:'00:46:03', mag:'closed', mq2:'danger', pir1:'yes', pir2:'yes'},
+      {hhmmss:'00:46:00', mag:'closed', mq2:'normal', pir1:'no', pir2:'no'},
+      {hhmmss:'00:46:01', mag:'open', mq2:'warning', pir1:'no', pir2:'yes'},
+      {hhmmss:'00:46:02', mag:'closed', mq2:'danger', pir1:'yes', pir2:'no'},
+      {hhmmss:'00:46:03', mag:'closed', mq2:'danger', pir1:'yes', pir2:'yes'},
+      {hhmmss:'00:46:00', mag:'closed', mq2:'normal', pir1:'no', pir2:'no'},
+      {hhmmss:'00:46:01', mag:'open', mq2:'warning', pir1:'no', pir2:'yes'},
+      {hhmmss:'00:46:02', mag:'closed', mq2:'danger', pir1:'yes', pir2:'no'},
+      {hhmmss:'00:46:03', mag:'closed', mq2:'danger', pir1:'yes', pir2:'yes'},
+      {hhmmss:'00:46:00', mag:'closed', mq2:'normal', pir1:'no', pir2:'no'},
+      {hhmmss:'00:46:01', mag:'open', mq2:'warning', pir1:'no', pir2:'yes'},
+      {hhmmss:'00:46:02', mag:'closed', mq2:'danger', pir1:'yes', pir2:'no'},
+      {hhmmss:'00:46:03', mag:'closed', mq2:'danger', pir1:'yes', pir2:'yes'},
+    ]
   },
   mutations: {
   },
