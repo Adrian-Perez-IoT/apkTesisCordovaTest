b401027249cebdb4d689b90121e32c0406ece16e
bandydos <perezjadrian@gmail.com>
2019-11-03 21:58:41 -0300

    Se soluciono el problema con la carga de la pagina de inicio

---
+++ src/views/Inicio.vue
@@ -0,0 +1,15 @@
+
+<template>
+    <div>
+        
+        
+            <h1>Aqui va el diseño mi pantalla de inicio (pantalla bienvenida de la app) </h1>
+        
+        
+    </div>
+</template>
+<script>
+export default {
+    name: "Inicio"
+}
+</script>
